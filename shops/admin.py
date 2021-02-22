6dd66d5814b2594ff98fc58186546e5632acefe2
marqisd <60140843+marqisd@users.noreply.github.com>
2021-02-22 23:20:30 +0000

    Code Cleanup

--- shops/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Shop
-from django.contrib import admin
-
-class ShopAdmin(OSMGeoAdmin):
-    list_display = ('name', 'location')
-
-admin.site.register(Shop, ShopAdmin)
