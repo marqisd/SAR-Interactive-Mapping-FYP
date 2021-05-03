e7191edeac9ae4f50befc783b38667dc7304c83b
marqisd <60140843+marqisd@users.noreply.github.com>
2021-05-03 04:07:48 +0100

    add features
    large update, add project save/load with database models and menus. add landing page. add probability ranges with graphical layers and data tables. add project form. add grid layer and grid segmenting with size option menu

--- geodjango/views.py
+++ geodjango/views.py
@@ -3,14 +3,39 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import psycopg2
 from django.template import loader
 from tracking.models import *
+import string
+import random
+
 
 def mapdemo(request):
     return render(request, 'mapdemo.html')
     
-def main(request):
+def details(request):
+    if request.method == 'POST':
+        form = request.POST
+        print(form.get('notes'))
+        name = random.choices(string.ascii_uppercase,k=5)
+        name = ''.join(name)
+        projForm = projectProfile(name=name,ipp=form.get('ipp'),searchClass=form.get('class'),notes=form.get('notes'))
+        projForm.save()
+        return HttpResponseRedirect('/main/'+name)
+    else:
+        return render(request, 'details.html')
+
+def main(request,projname):
     trackPoints = trackingPoint.objects.all()
-    args={'trackPoints':trackPoints}
+    projectData= projectProfile.objects.filter(name=projname)
+    args={'trackPoints':trackPoints,'projData':projectData}
     return render(request, 'main.html',args)
 
 def landing(request):
-    return render(request, 'landing.html')
+    if request.method == 'POST':
+        form = request.POST
+        name =form.get('projCode')
+        projectData= projectProfile.objects.filter(name=name)
+        if len(projectData) != 0 :
+            return HttpResponseRedirect('/main/'+name)
+        else:
+            return render(request, 'landing.html')
+    else:
+        return render(request, 'landing.html')
--- tracking/models.py
+++ tracking/models.py
@@ -7,4 +7,13 @@ class trackingPoint(models.Model):
     Lon = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.Lat
+        return self.name
+
+class projectProfile(models.Model):
+    name = models.CharField(max_length=500, default='hello')
+    ipp = models.CharField(max_length=500, default='NA')
+    searchClass = models.CharField(max_length=500, default='NA')
+    notes = models.TextField(default='NA')
+
+    def __str__(self):
+        return self.name
